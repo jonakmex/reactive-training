b114df9e0291b350340f980e1f2f675f2284c8e7
Jonathan Bustamante <jonakmex@gmail.com>
2021-12-27 14:32:31 -0500

    Spring cloud contract sample initial state

---
+++ 01-spring-cloud-contract-producer/src/test/java/com/example/springcloudcontractproducer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcontractproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
